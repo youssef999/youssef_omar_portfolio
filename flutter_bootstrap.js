2aaafa5eaeaed7aa208e0d4bcf9e803e6c8a2381
youssef999 <youssef13579r@gmail.com>
2026-03-01 22:37:52 +0200

    AAA

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"453dd7174cc6555e3e9eb5f006c86e6ec221a0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4181240567"
+    serviceWorkerVersion: "1906013101"
   }
 });
